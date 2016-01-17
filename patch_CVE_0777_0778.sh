223d0b14bfd859e01be2ed2db52fe1c64936e291
bucio <bucio@users.noreply.github.com>
2016-01-17 02:28:40 -0600

    Update patch_CVE_0777_0778.sh

--- patch_CVE_0777_0778.sh
+++ patch_CVE_0777_0778.sh
@@ -14,6 +14,8 @@ case `uname` in
      LINUX=1
      which yum && { echo "";echo "Patch.. "; echo 'UseRoaming no' | sudo tee -a /etc/ssh/ssh_config; echo 'Patch ok ..'; echo''; return; }
      which apt-get && { echo "";echo "Patch.. "; echo 'UseRoaming no' | sudo tee -a /etc/ssh/ssh_config; echo 'Patch ok ..'; echo''; return; }
+     #which pkg && { echo "";echo "Patch.. "; echo -e 'Host *\nUseRoaming no' >> /etc/ssh/ssh_config; echo 'Patch ok ..'; echo''; return; }
+
      ;;
   Darwin )
      DARWIN=1
@@ -25,6 +27,7 @@ case `uname` in
      ;;
   * )
      echo ""
+     #which pkg && { echo "";echo "Patch.. "; echo -e 'Host *\nUseRoaming no' >> /etc/ssh/ssh_config; echo 'Patch ok ..'; echo''; return; }
      echo "Este script, es para debian y centOS, si usas un sistema difernte añade tu distro en la linea 16 :)"
      echo ""
      ;;
@@ -38,7 +41,14 @@ case `uname` in
      LINUX=1
      which yum && { echo '##################'; echo 'Checa si estas en el rango de estas versiones 5.4 a 5.7';echo ''; yum list installed openssh\*; echo 'Si estas en ese rango presiona 2 !! '; echo "#####################################"; echo "";return; }
      which apt-get && { echo '##################'; echo 'Checa si estas en el rango de estas versiones 5.4 a 5.7';echo ''; dpkg --list openssh\*; echo 'Si estas en ese rango presiona 2 !! '; echo "#####################################"; echo"";return; }
+     
      ;;
+
+#  BSD )
+#    BSD=1
+#    which yum && { echo '##################'; echo 'Checa si estas en el rango de estas versiones 5.4 a 5.7';echo ''; yum list installed openssh\*; echo 'Si estas en ese rango presiona 2 !! '; echo "#####################################"; echo "";return; }   
+#    ;;
+
   Darwin )
      DARWIN=1
      echo ""
@@ -81,7 +91,11 @@ echo '
     /_/                                         
 '
 
-echo "Porfavor elige una opcion: "
+echo "openSSH Information Leak (CVE-2016-0777)"
+echo "openSSH Buffer Overflow (CVE-2016-0778)"
+echo "All OpenSSH versions between 5.4 and 7.1 are vulnerable."
+
+echo "Elige una opcion: "
 echo " "
 menu=("Checar si soy vulnerable" "Parchar" "info" "Salir")
 select opt in "${menu[@]}"
@@ -90,14 +104,16 @@ do
     	"Checar si soy vulnerable")
 			echo ""
             echo "Si tienes una versión entre 5.4 y la 7.1 eres vulnerable"
-			check_version
+			     check_version
+            echo ""
             ;;  
 
         "Parchar")
 			echo ""
             echo "Parchando"
             patch_s
-			echo ""
+            echo ""
+			       echo ""
             ;;
 
 
@@ -106,10 +122,14 @@ do
 			echo ""
             echo "Si necesitas más info sobre estos CVE."
             echo "URL: https://www.qualys.com/2016/01/14/cve-2016-0777-cve-2016-0778/openssh-cve-2016-0777-cve-2016-0778.txt"
-            			echo ""
+            echo "URL http://seclists.org/fulldisclosure/2016/Jan/44"
+            echo "URL: https://www.cvedetails.com/cve-details.php?t=1&cve_id=CVE-2016-0777"
+            echo "URL: https://www.cvedetails.com/cve-details.php?t=1&cve_id=CVE-2016-0778"
+            echo ""
             ;;
 
         "Salir")
+          echo "Adios"
             break
             ;;
         *) echo Opcion invalida;;
